3d68288442954c3ca6a892a2d8ec155d655e5e2b
Caroline Rives <car.rives@gmail.com>
2022-07-12 16:32:25 -0400

    JUnit lab complete

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,19 @@
 public class FizzBuzz {
+/*
+This code has 2 bugs! Write unit tests to test the logic and find the bugs - here are the steps:j
+
+Test the "starts with f" case first - this should pass.
+Test the "ends with b" case - this should also pass.
+Test the "starts with f and ends with b" case - this will not pass.
+Fix the logic in the solution so that all 3 test cases pass.
+Add a test for when str is null - this will not pass.
+Fix the logic in the solution so that all 4 test cases pass.
+ */
   public String fizzBuzzString(String str) {
+      if (str == null) return null;
+      if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
       if (str.startsWith("f"))  return "Fizz";
       if (str.endsWith("b")) return "Buzz";
-
-      if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
 
       return str;
   }
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void fizzBuzzString() {
+        String testString = "foe";
+        String test = fizzBuzz.fizzBuzzString(testString);
+        assertEquals("Fizz", test);
+
+        String testString2 = "bob";
+        String test2 = fizzBuzz.fizzBuzzString(testString2);
+        assertEquals("Buzz", test2);
+
+        String testString3 = "fob";
+        String test3 = fizzBuzz.fizzBuzzString(testString3);
+        assertEquals("FizzBuzz", test3);
+
+        String testString4 = null;
+        String test4 = fizzBuzz.fizzBuzzString(testString4);
+        assertNull(test4);
+    }
+}
